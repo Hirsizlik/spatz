a102607018c06dfca31ebe3dec50d54fc8290e26
Markus Schagerl <Markus.Schagerl1@gmx.at>
2026-02-10 14:02:44 +0100

    Simplified webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,37 @@
 import { resolve } from 'path';
 import nodeExternals from 'webpack-node-externals';
 
-export default {
+const client = {
+  entry: './client/index.tsx',
+  output: {
+    path: resolve(import.meta.dirname, './public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+};
+
+const server = {
   entry: './server/server.tsx',
   target: 'node22',
   externals: [nodeExternals({ importType: "module" })],
@@ -27,5 +57,7 @@ export default {
   },
   experiments: {
     outputModule: true,
-  },
+  }
 };
+
+export default [server, client];
